cae0599118987f4c38b7b0ee94d438f894eee185
Joshuathomas15 <jjulyo6mdls958@gmail.com>
2025-01-26 17:26:20 -0500

    Add project files.

---
+++ chapter 1 program 1-1.cpp
@@ -0,0 +1,39 @@
+// chapter 1 program 1-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// 
+// 
+// 
+//   1.// this program calculates the use 
+//   2. #include <iostream>
+//   3. using namespace std
+//   4. 
+//   5. int main()
+//   6.
+//   7. double hours, rate, pay;
+//   8.
+//   9.// get the number of hours worked
+//  10. cout << "how many hours did you work? ";
+//  11. cin >> hours;
+//  12
+//  13 // Get the hourly pay rate.
+//  14 cout << " How much do you get paid per hour? ";
+//  15 cin >> rate;
+//  16
+//  17 // calculate the pay
+//  18 pay=hours*rate;
+//  19
+//  20 // Display the pay.
+//  21  cout << "you have earned $" << pay << endl;
+//  22 return 0;
+//  23 } 
